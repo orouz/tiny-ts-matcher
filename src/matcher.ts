390a363b01530e6d1f4282823132cb8f5d3d1f5f
Or Ouziel <ouzielor@gmail.com>
2021-02-13 05:48:06 +0200

    style(matcher): add comments ✏

--- src/matcher.ts
+++ src/matcher.ts
@@ -10,18 +10,28 @@
  * })
  */
 export function createMatcher<TagKey extends string>(key: TagKey) {
+  // ^^ TagKey is the discriminator of our discriminated union
   return <TaggedUnion extends { [E in TagKey]: string | number }>() => {
+    // ^^ TaggedUnion is our discriminated union
     return <
       Cases extends {
+        // ^^ Cases is our handlers object (like switch cases)
         [P in TaggedUnion[TagKey]]: (
+          // ^^ verify all keys are specified in Cases (although it will take more)
           v: TaggedUnion & { [E in TagKey]: P }
+          // ^^ use the correct union type as the handler argument type by narrowing down TaggedUnion
         ) => any;
+        // ^^ we can return anything from each case, it will be inferred later
       } & { _?: (v: unknown) => any }
+      // ^^ use "_" as the default case handler
     >(
       cases: Cases
     ) => <Value extends TaggedUnion>(
+      // ^^ the value to match against
       v: Value
     ): ReturnType<Cases[Value[TagKey]]> =>
+      // ^^ Get the value from Cases given the TagKey of Value
       cases[v[key]]?.(v) ?? cases["_"]?.(v);
+    // ^^ return the value from a case handler or the default handler, undefined if none return a value
   };
 }
