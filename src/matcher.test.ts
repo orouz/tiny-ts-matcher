178b6919a8f9fbb08bf57382b8c9ee644db88d96
Or Ouziel <ouzielor@gmail.com>
2021-02-13 04:54:24 +0200

    chore(matcher): clean up (#3)

--- src/matcher.test.ts
+++ src/matcher.test.ts
@@ -8,25 +8,25 @@ describe("match", () => {
       | { status: 400; error: string };
 
     const match500 = match<ServerResponse>()({
-      500: ({ message }) => `Server Error - ${message}`,
+      500: ({ message }) => message,
       400: ({ error }) => error,
       _: () => "no match",
     })({ status: 500, message: "woops" });
 
     const match400 = match<ServerResponse>()({
-      500: ({ message }) => `Server Error - ${message}`,
+      500: ({ message }) => message,
       400: ({ error }) => error,
       _: () => "no match",
     })({ status: 400, error: "bad req" });
 
     const match_ = match<ServerResponse>()({
-      500: ({ message }) => `Server Error - ${message}`,
+      500: ({ message }) => message,
       400: ({ error }) => error,
       _: () => "no match",
     })({} as any);
 
-    expect(match500).toBe(`Server Error - woops`);
-    expect(match400).toBe(`bad req`);
-    expect(match_).toBe(`no match`);
+    expect(match500).toBe("woops");
+    expect(match400).toBe("bad req");
+    expect(match_).toBe("no match");
   });
 });
