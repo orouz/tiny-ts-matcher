adcb150e0d0fa76082ebd1e8a1f58ff12514f23a
Or Ouziel <ouzielor@gmail.com>
2021-04-02 16:02:08 +0300

    fix: add string|number  key type

--- src/matcher.test.ts
+++ src/matcher.test.ts
@@ -1,7 +1,7 @@
 import { createMatcher } from "./matcher";
 
 describe("check matcher calls correct handlers", () => {
-  it("calls handlers for keys: 400, 500 and _", async () => {
+  it("calls handlers for numeric keys: 400, 500 and _", async () => {
     const match = createMatcher("status");
     type ServerResponse =
       | { status: 500; message: string }
@@ -23,11 +23,46 @@ describe("check matcher calls correct handlers", () => {
       500: ({ message }) => message,
       400: ({ error }) => error,
       _: () => "no match",
-    })({} as any);
+    })({ type: 500 } as any);
 
     const match_none = match<ServerResponse>()({
       500: ({ message }) => message,
       400: ({ error }) => error,
+    })({} as any);
+
+    expect(match500).toBe("woops");
+    expect(match400).toBe("bad req");
+    expect(match_).toBe("no match");
+    expect(match_none).toBe(undefined);
+  });
+
+  it("calls handlers for string keys: 400, 500 and _", async () => {
+    const match = createMatcher("status");
+    type ServerResponse =
+      | { status: "500"; message: string }
+      | { status: "400"; error: string };
+
+    const match500 = match<ServerResponse>()({
+      "500": ({ message }) => message,
+      "400": ({ error }) => error,
+      _: () => "no match",
+    })({ status: "500", message: "woops" });
+
+    const match400 = match<ServerResponse>()({
+      "500": ({ message }) => message,
+      "400": ({ error }) => error,
+      _: () => "no match",
+    })({ status: "400", error: "bad req" });
+
+    const match_ = match<ServerResponse>()({
+      "500": ({ message }) => message,
+      "400": ({ error }) => error,
+      _: () => "no match",
+    })({ type: "500" } as any);
+
+    const match_none = match<ServerResponse>()({
+      "500": ({ message }) => message,
+      "400": ({ error }) => error,
     })({} as any);
 
     expect(match500).toBe("woops");
