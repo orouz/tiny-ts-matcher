b32a9d18896350f8b1e98f04fa1ad6e6db289f46
Or Ouziel <ouzielor@gmail.com>
2021-04-03 11:21:33 +0300

    refactor: replace any with unknown

--- src/matcher.test.ts
+++ src/matcher.test.ts
@@ -1,7 +1,7 @@
 import { createMatcher } from "./matcher";
 
 describe("check matcher calls correct handlers", () => {
-  it("calls handlers for numeric keys: 400, 500 and _", async () => {
+  it("calls handlers for string/numeric keys: 400, 500 and _", async () => {
     const match = createMatcher("status");
     type ServerResponse =
       | { status: 500; message: string }
@@ -34,40 +34,17 @@ describe("check matcher calls correct handlers", () => {
     expect(match400).toBe("bad req");
     expect(match_).toBe("no match");
     expect(match_none).toBe(undefined);
-  });
 
-  it("calls handlers for string keys: 400, 500 and _", async () => {
-    const match = createMatcher("status");
-    type ServerResponse =
+    type ServerResponseStr =
       | { status: "500"; message: string }
       | { status: "400"; error: string };
 
-    const match500 = match<ServerResponse>()({
-      "500": ({ message }) => message,
-      "400": ({ error }) => error,
-      _: () => "no match",
-    })({ status: "500", message: "woops" });
-
-    const match400 = match<ServerResponse>()({
-      "500": ({ message }) => message,
-      "400": ({ error }) => error,
+    const match400s = match<ServerResponseStr>()({
+      500: ({ message }) => message,
+      400: ({ error }) => error,
       _: () => "no match",
     })({ status: "400", error: "bad req" });
 
-    const match_ = match<ServerResponse>()({
-      "500": ({ message }) => message,
-      "400": ({ error }) => error,
-      _: () => "no match",
-    })({ type: "500" } as any);
-
-    const match_none = match<ServerResponse>()({
-      "500": ({ message }) => message,
-      "400": ({ error }) => error,
-    })({} as any);
-
-    expect(match500).toBe("woops");
-    expect(match400).toBe("bad req");
-    expect(match_).toBe("no match");
-    expect(match_none).toBe(undefined);
+    expect(match400s).toBe("bad req");
   });
 });
